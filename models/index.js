f6dc3b09ed1facc3323b78e5186f08bcc04b67e5
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-08-09 17:08:57 -0500

    fixed models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,9 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
